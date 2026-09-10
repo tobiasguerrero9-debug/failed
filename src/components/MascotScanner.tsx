91306fa74d05bdd81187110c3088f5a8e2ee6ff7
Tobias Guerrero <tobiasguerrero9@gmail.com>
2026-09-10 11:03:13 -0500

    fix: replace mascot asset with 100% transparent PNG with zero background canvas

--- src/components/MascotScanner.tsx
+++ src/components/MascotScanner.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export const MascotScanner: React.FC = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center w-full max-w-[580px] sm:max-w-[660px] lg:max-w-[700px] mx-auto select-none">
+    <div className="relative flex flex-col items-center justify-center w-full max-w-[560px] sm:max-w-[640px] lg:max-w-[680px] mx-auto select-none">
       
-      {/* Soft Bottom Floor Contact Shadow */}
-      <div className="absolute bottom-5 left-1/2 -translate-x-1/2 w-[75%] h-14 bg-[#1D103A]/20 rounded-[100%] blur-xl pointer-events-none" />
+      {/* Soft Bottom Floor Shadow Pool */}
+      <div className="absolute -bottom-2 left-1/2 -translate-x-1/2 w-[70%] h-12 bg-[#1D103A]/20 rounded-[100%] blur-xl pointer-events-none" />
 
-      {/* 3D Computer Mascot */}
+      {/* High-Resolution 100% Transparent 3D Computer Mascot */}
       <div className="relative z-10 w-full flex justify-center">
         <img
           src="/assets/mascot-v2.png"
-          alt="FAILED 3D Computer Mascot"
-          className="w-full h-auto object-contain drop-shadow-[0_25px_50px_rgba(30,16,58,0.2)]"
+          alt="FAILED 3D Retro Computer Mascot"
+          className="w-full h-auto object-contain bg-transparent border-0 outline-none select-none"
           loading="eager"
           decoding="async"
         />
@@ -21,10 +21,10 @@ export const MascotScanner: React.FC = () => {
         <div 
           className="absolute rounded-[12%] flex flex-col items-center justify-center text-center overflow-hidden pointer-events-none"
           style={{
-            top: '21.5%',
-            left: '32%',
-            width: '37%',
-            height: '31%'
+            top: '20.5%',
+            left: '27%',
+            width: '42.5%',
+            height: '33%'
           }}
         >
           {/* Green Pixel Smile Only */}
